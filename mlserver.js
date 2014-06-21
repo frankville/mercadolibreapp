f449b4b19bece1373a98ba684977a4bce59e4ca8
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-06-21 11:28:16 +0000

    changed user creds

--- mlserver.js
+++ mlserver.js
@@ -9,7 +9,7 @@ var accessToken = "";
 var app = express();
 
 
-var mlObj = new mercadolibre.Meli(6784136706596646,"NRvN4zavRkazjmi1ZDGHUw6jEw1wOPV2",null,null);
+var mlObj = new mercadolibre.Meli(891762901822103,"sAqsXAaIvq78CtzYRSwyIBLFJZYd2vbD",null,null);//app keys from mltestuser account
 var redirURI  = "https://devcloud.dnsdynamic.com/auth";
 
 
@@ -23,33 +23,29 @@ app.get("/", function(req, res){
 });
 
 
-
 function getUserInfo(){
 	mlObj.get("/users/me",{access_token: accessToken}, function(error,response){
 		console.log(error);
-		console.log(response);
+		console.log(response.last_name+", "+response.first_name);
 	});
 }
 
-app.get("/user", function(req,res){
-	mlObj.get("/users/me",{access_token: accessToken}, function(error,response){
-		console.log(error);
-		res.send(response);
-	});
-});
 
 app.get("/auth", function(req,res){
 	mlObj.authorize(req.query.code,redirURI, function(error,response){
 		accessToken = response.access_token;
-		console.log(accessToken);
 		setInterval(getUserInfo,5000);//ejecuta la funcion getUserInfo cada 5 segs
+		res.redirect("https://devcloud.dnsdynamic.com");
 	});
 
 });
 
 app.get("/notifs", function(req,res){
 	console.log(req.query);
-})
+});
+app.get("/dashboard", function(req,res){
+	
+});
 
 app.get("/ingresar", function(req,res){
 
