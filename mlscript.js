4de7ccda78673ca9ebafb07cf25e902a0330f5b2
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-06-22 05:02:11 +0000

    added mysql capabilities

--- mlscript.js
+++ mlscript.js
@@ -1,32 +1,17 @@
 $(document).ready(function()
 {
-	/*
-		MELI.init({client_id: app_id
-});
 
-MELI.init({client_id: app_id,
-    xauth_protocol: "https://",
-	xauth_domain: "secure.mlstatic.com",
-	xd_url: "/org-img/sdk/xd-1.0.4.html"
-});
-})
-*/
 
 });
 
 function ingresar(){
-/*
-	MELI.login(function(){
-		MELI.get("/users/me",null, function(data){
-			showUserName(JSON.stringify(data[2]));
-			console.log("valor "+JSON.stringify(data[2]));
-		});
-	});
-*/
+
 $.get("/ingresar", function(data){
 	console.log(data);
 	window.location.href = data;
-	//redireccionar
-			//showUserName(data);
+	$.get("/userinfo", function(resp){
+		console.log("user: "+resp);
+	});
+
 });
 };
--- mlserver.js
+++ mlserver.js
@@ -3,11 +3,41 @@ var https = require("https");
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
-//var querystring = require("querystring");
+var mysql = require("mysql");
 var mercadolibre = require("mercadolibre");
 var accessToken = "";
 var app = express();
+var connqty = 0;//amount of connected clients (max)
 
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "1",
+    password:"1",
+    database: "fashion"
+});
+
+
+
+db.connect(function(err){
+	if(err){
+		console.log(err);
+	}
+});
+
+function getArticles(){
+//obtiene todo los articulos de la empresa 002
+db.query("SELECT codigoarticulo, descripcion, inventario,"+
+ "activo, preciopublico, talla, color, modelo , marca FROM fashion.articulos where codigoempresa = 2; ", function(err, result){
+ 	if(!err){
+ 		//console.log(result);
+ 		https.get("/listarticles",{list: result}, function(respdata){
+
+ 		});
+ 	}else{
+ 		console.log("error "+err); 	
+ 	}
+ });
+};
 
 var mlObj = new mercadolibre.Meli(891762901822103,"sAqsXAaIvq78CtzYRSwyIBLFJZYd2vbD",null,null);//app keys from mltestuser account
 var redirURI  = "https://devcloud.dnsdynamic.com/auth";
@@ -22,11 +52,28 @@ app.get("/", function(req, res){
 
 });
 
+function retrieveDBObjects(){
+	getUserInfo();
+	getArticles();
+}
 
-function getUserInfo(){
+app.get("/listarticles", function(req,res){
+	//res.send(req.params.list);
+});
+
+app.get("/userinfo", function(req,res){
+	getUserInfo(function(uname){
+		res.send(uname);
+	});
+
+
+});
+
+
+function getUserInfo(callback){
 	mlObj.get("/users/me",{access_token: accessToken}, function(error,response){
-		console.log(error);
-		console.log(response.last_name+", "+response.first_name);
+		console.log("error!" +error);
+		callback(response.last_name+", "+response.first_name);
 	});
 }
 
@@ -34,7 +81,7 @@ function getUserInfo(){
 app.get("/auth", function(req,res){
 	mlObj.authorize(req.query.code,redirURI, function(error,response){
 		accessToken = response.access_token;
-		setInterval(getUserInfo,5000);//ejecuta la funcion getUserInfo cada 5 segs
+		//setInterval(retrieveDBObjects,5000);//ejecuta la funcion getUserInfo cada 5 segs
 		res.redirect("https://devcloud.dnsdynamic.com");
 	});
 
